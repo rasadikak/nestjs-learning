a8f28f387fbd0f383cec85737a048c0e09a7dc16
kaushani rasadika <rasadikak@gmail.com>
2026-09-23 10:41:07 +0530

    auth module

--- src/auth/Auth.controller.ts
+++ src/auth/Auth.controller.ts
@@ -1,5 +1,18 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
+import { AuthService } from "./Auth.service";
 
 
-@Controller()
-export class AuthController{}
+@Controller('auth')
+export class AuthController{
+
+    constructor(private AuthService: AuthService){}
+
+    @Post('signup')
+    signup(){
+        return this.AuthService.signup();
+    }
+
+
+    @Post('signin')
+    signin(){}
+}
--- src/auth/Auth.service.ts
+++ src/auth/Auth.service.ts
@@ -2,4 +2,14 @@ import { Injectable } from "@nestjs/common";
 
 
 @Injectable({})
-export class AuthService{}
+export class AuthService{
+    signin(){
+
+    }
+
+    signup(){
+        return {msg:'i have signed up'};
+
+    }
+}
+
